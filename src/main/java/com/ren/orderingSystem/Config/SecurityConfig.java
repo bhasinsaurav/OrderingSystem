7be91772d829e05ae54b62261c6c24255343de3b
Saurav Bhasin <sauravbhasin@Sauravs-MacBook-Air.local>
2025-08-07 16:57:33 -0400

    All origins allowed

--- src/main/java/com/ren/orderingSystem/Config/SecurityConfig.java
+++ src/main/java/com/ren/orderingSystem/Config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of(frontendLocalhostUrl, cloudflareurl,"http://localhost:5174")); // Accept all origins
+        configuration.setAllowedOriginPatterns(List.of("*")); // Accept all origins
         configuration.setAllowedMethods(List.of("*")); // All typical HTTP methods
         configuration.setAllowedHeaders(List.of("*")); // Accept all headers
         configuration.setAllowCredentials(true); // Accept cookies, tokens, etc.
